2fb47c6a34f548d414c730389d0a4a811418978a
rachelAdams <rachel.adams1012@gmail.com>
2016-06-28 14:49:39 -0700

    Initial commit of package for SSO

---
+++ SSO_Service_Offering/wwwroot/app/service/blob.service.ts
@@ -0,0 +1,16 @@
+﻿import {Injectable} from "angular2/core";
+
+@Injectable()
+export class BlobService {
+    private blobUrl = "https://mscspemsprovoketestblob.blob.core.windows.net/csp";
+
+    constructor() { }
+
+    setValue(val) {
+        this.blobUrl = val;
+    }
+
+    getValue() {
+        return this.blobUrl;
+    }
+}
