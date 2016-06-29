97047d710d18140da21bf1b58620bba73863a7ba
JC Ansus <jc.ansus@outlook.com>
2016-06-29 12:32:08 +0800

    Initial commit for MDM and MFA base code

---
+++ MFA_Service_Offering/wwwroot/app/model/illustration.ts
@@ -0,0 +1,8 @@
+﻿import {IllustrationImage} from "./illustration-image";
+
+export class Illustration {
+    id: number;
+    location: string; //top or bottom
+    header: string;
+    images: IllustrationImage[];
+}
